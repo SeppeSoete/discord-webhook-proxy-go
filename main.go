0246939eaabc02d755f76c84b3629def1cdfa00c
Seppe Soete <info@seppesoete.be>
2023-08-13 08:52:13 +0200

    adding and deleting user tokens from firebase

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
 	"errors"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+
+	"cloud.google.com/go/firestore"
+	firebase "firebase.google.com/go"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
+type datastoreObject struct {
+	Name  string
+	Admin bool
+}
+
 func main() {
+
 	// default error handler in main function: if something goes wrong, abort mission
 	handleErr := func(err error) {
 		if err != nil {
@@ -24,7 +38,13 @@ func main() {
 	proxy, err := mkProxy(webhook)
 	handleErr(err)
 
-	http.HandleFunc("/", mkServer(proxy))
+	ctx := context.Background()
+	client := mkFirestoreClient(ctx)
+	defer client.Close()
+
+	http.HandleFunc("/newToken", handleNewToken(client, mkValidator(client, true)))
+	http.HandleFunc("/deleteUser", handleDeleteRequest(client, mkValidator(client, true)))
+	http.HandleFunc("/", mkServer(proxy, mkValidator(client, false)))
 
 	handleErr(http.ListenAndServe(":"+port, nil))
 }
@@ -61,17 +81,133 @@ func mkProxy(dest string) (*httputil.ReverseProxy, error) {
 }
 
 // Make the http server that handles all incoming requests to /
-func mkServer(proxy *httputil.ReverseProxy) func(w http.ResponseWriter, r *http.Request) {
+func mkServer(proxy *httputil.ReverseProxy, validator func(token string) bool) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		q := r.URL.Query()
-		if !validPassword(q.Get("password")) {
+		if !validator(q.Get("token")) {
+			w.WriteHeader(http.StatusUnauthorized)
 			return
 		}
 		proxy.ServeHTTP(w, r)
 	}
 }
 
+func mkFirestoreClient(ctx context.Context) *firestore.Client {
+	// Sets your Google Cloud Platform project ID.
+	projectID := os.Getenv("PROJECT_ID")
+	if projectID == "" {
+		log.Fatalf("no project ID provided")
+	}
+
+	conf := &firebase.Config{ProjectID: projectID}
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	// Close client when done with
+	// defer client.Close()
+	return client
+}
+
+func generateToken(length int) string {
+	b := make([]byte, length)
+	if _, err := rand.Read(b); err != nil {
+		return ""
+	}
+	return hex.EncodeToString(b)
+}
+
 // Validate a password, currently hardcoded, but should be replaced by a user token
-func validPassword(password string) bool {
-	return password == "supersecretpassword"
+func mkValidator(client *firestore.Client, admin bool) func(token string) bool {
+	return func(token string) bool {
+		user := retrieveUserObjectByToken(client, token)
+		if user.Name == "" {
+			return false
+		}
+		if admin && !user.Admin {
+			return false
+		}
+		return true
+	}
+}
+
+func retrieveUserObjectByToken(client *firestore.Client, token string) datastoreObject {
+	ctx := context.Background()
+	obj, err := client.Collection("users").Doc(token).Get(ctx)
+	user := datastoreObject{}
+	if status.Code(err) == codes.NotFound {
+		return user
+	}
+	if err := obj.DataTo(datastoreObject{}); err != nil {
+		user = datastoreObject{}
+	}
+	return user
+}
+
+func deleteUser(client *firestore.Client, name string) error {
+	ctx := context.Background()
+	queries, err := client.Collection("users").Where("name", "==", name).Documents(ctx).GetAll()
+	if err != nil {
+		return err
+	}
+	for i := range queries {
+		queries[i].Ref.Delete(ctx)
+	}
+	return nil
+}
+
+func handleDeleteRequest(client *firestore.Client, validator func(token string) bool) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		q := r.URL.Query()
+		name := q.Get("name")
+		token := q.Get("token")
+		if name == "" || token == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if !validator(token) {
+			log.Println("failed to validate admin request")
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		err := deleteUser(client, name)
+		if err != nil {
+			log.Println("could not delete user: ", err)
+			w.WriteHeader(500)
+			return
+		}
+	}
+}
+
+// New token request
+func handleNewToken(client *firestore.Client, validator func(token string) bool) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		q := r.URL.Query()
+		if !validator(q.Get("token")) {
+			log.Println("failed to validate admin request")
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		name := q.Get("name")
+		if name == "" {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		token := generateToken(10)
+		ctx := context.Background()
+		obj := datastoreObject{name, false}
+		log.Printf("making new client with name %s and token %s\n", name, token)
+		_, err := client.Collection("users").Doc(token).Set(ctx, obj)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(500)
+		}
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(token))
+
+	}
 }
